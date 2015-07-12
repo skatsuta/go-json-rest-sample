15cce376077ca0135ee0da54b64063b5ad5a9546
Soshi Katsuta <katsuta_soshi@cyberagent.co.jp>
2015-07-12 23:09:53 +0900

    main: add country API

--- main.go
+++ main.go
@@ -5,13 +5,21 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"sync"
 
 	"github.com/ant0ine/go-json-rest/rest"
 )
 
 func main() {
-	port := flag.String("port", "9999", "port to listen")
+	var port string
+	flag.StringVar(&port, "port", "9999", "port to listen")
 	flag.Parse()
+
+	log.Println("Starting server on localhost:" + port)
+
+	countries := Countries{
+		Store: map[string]*Country{},
+	}
 
 	api := rest.NewApi()
 	api.Use(rest.DefaultDevStack...)
@@ -31,10 +39,99 @@ func main() {
 				log.Println(e)
 			}
 		}),
+		rest.Get("/countries", countries.GetAllCountries),
+		rest.Post("/countries", countries.PostCountry),
+		rest.Get("/countries/:code", countries.GetCountry),
+		rest.Delete("/countries/:code", countries.DeleteCountry),
 	)
 	if err != nil {
 		log.Fatal(err)
 	}
 	api.SetApp(router)
-	log.Fatal(http.ListenAndServe(":"+*port, api.MakeHandler()))
+	log.Fatal(http.ListenAndServe(":"+port, api.MakeHandler()))
+}
+
+// Country is a country.
+type Country struct {
+	Code string
+	Name string
+}
+
+// Countries is a collection of countries.
+type Countries struct {
+	sync.RWMutex
+	Store map[string]*Country
+}
+
+// GetCountry returns a country corresponding the given country code.
+func (c *Countries) GetCountry(w rest.ResponseWriter, r *rest.Request) {
+	code := r.PathParam("code")
+
+	c.RLock()
+	var country *Country
+	if c.Store[code] != nil {
+		country = &Country{}
+		*country = *c.Store[code]
+	}
+	c.RUnlock()
+
+	if country == nil {
+		rest.NotFound(w, r)
+		return
+	}
+	returnJSON(w, &country)
+}
+
+// GetAllCountries returns all countries registered.
+func (c *Countries) GetAllCountries(w rest.ResponseWriter, r *rest.Request) {
+	c.RLock()
+
+	countries := make([]Country, len(c.Store))
+	i := 0
+	for _, country := range c.Store {
+		countries[i] = *country
+		i++
+	}
+	c.RUnlock()
+	returnJSON(w, countries)
+}
+
+// PostCountry adds a posted code and countriy.
+func (c *Countries) PostCountry(w rest.ResponseWriter, r *rest.Request) {
+	country := Country{}
+	if e := r.DecodeJsonPayload(&country); e != nil {
+		rest.Error(w, e.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if country.Code == "" {
+		rest.Error(w, "country code required", 400)
+		return
+	}
+
+	if country.Name == "" {
+		rest.Error(w, "country name required", 400)
+		return
+	}
+
+	c.Lock()
+	c.Store[country.Code] = &country
+	c.Unlock()
+
+	returnJSON(w, &country)
+}
+
+// DeleteCountry deletes a specified country.
+func (c *Countries) DeleteCountry(w rest.ResponseWriter, r *rest.Request) {
+	code := r.PathParam("code")
+	c.Lock()
+	delete(c.Store, code)
+	c.Unlock()
+	w.WriteHeader(http.StatusOK)
+}
+
+func returnJSON(w rest.ResponseWriter, v interface{}) {
+	if e := w.WriteJson(v); e != nil {
+		log.Println(e)
+	}
 }
